354e90dfd45cce8fdb2d62879cafcbc9db15b3b0
German Arciniegas <gear66me@gmail.com>
2026-07-25 19:52:59 -0500

    Update comments and import statement in GV-beta-0002E.py

--- viewer/GV-beta-0002E.py
+++ viewer/GV-beta-0002E.py
@@ -1,8 +1,8 @@
-from IPython.display import HTML, display
+From IPython.display import HTML, display
 
-# GV-beta-0002A
+# GV-beta-0002E
 # Standalone Galaxy Viewer release.
-# GV-beta-0002
+# GV-beta-0002E
 # Fixed-size Galaxy Viewer SIMBAD proxy; native Aladin control is hidden command engine.
 # No earlier Galaxy Viewer file is imported, downloaded, patched, or executed.
 
@@ -367,7 +367,8 @@ A.init.then(() => {
 
 });
 </script>
-"""))
+""")
 
-#// fix applied
-#// pass
+# fix applied
+# pass
+
