2e8b6ce922829e341b6546c15ad72ad67817bc05
German Arciniegas <gear66me@gmail.com>
2026-07-27 02:43:08 -0500

    Remove Galaxy Viewer PWA splash and caches for 0009

--- mobile/service-worker.js
+++ mobile/service-worker.js
@@ -1,21 +1,5 @@
-const CACHE_NAME='galaxy-viewer-mobile-0008';
-const APP_SHELL=['./','./index.html?v=0008','./manifest.webmanifest?v=0008','./icons/galaxy-viewer-0008.jpg','./assets/gv-mobile-splash-0008.jpg','./releases/GV-mobile-0008.html'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
-  self.skipWaiting();
-});
-
+self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));
-  self.clients.claim();
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.registration.unregister()).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
-    return response;
-  }).catch(()=>caches.match(event.request)));
-});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}))});
