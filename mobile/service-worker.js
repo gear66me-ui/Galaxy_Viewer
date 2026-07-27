9de49ead4088b02ade816a11952a4c102c4eb567
German Arciniegas <gear66me@gmail.com>
2026-07-27 15:14:13 -0500

    Enable Galaxy Viewer installable app service worker

--- mobile/service-worker.js
+++ mobile/service-worker.js
@@ -1,5 +1,35 @@
-self.addEventListener('install',event=>{self.skipWaiting()});
-self.addEventListener('activate',event=>{
- event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.registration.unregister()).then(()=>self.clients.claim()));
+const CACHE_NAME = 'galaxy-viewer-mobile-0015';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg',
+  '../viewer/artwork/GV-splash-0002.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
 });
-self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}))});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./')))
+  );
+});
