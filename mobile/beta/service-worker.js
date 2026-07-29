20ec6afdd86b1e6ef005d2dde476c4175f602060
German Arciniegas <gear66me@gmail.com>
2026-07-28 21:40:22 -0500

    Cache GV-beta-0005F and centered reticle in mobile beta

--- mobile/beta/service-worker.js
+++ mobile/beta/service-worker.js
@@ -1,11 +1,12 @@
-const CACHE_NAME='galaxy-viewer-beta-5E-iconfix-002';
+const CACHE_NAME='galaxy-viewer-beta-5F-reticle-001';
 const APP_SHELL=[
   './',
   './index.html',
-  './manifest.webmanifest?v=5E-iconfix-002',
-  '../../viewer/artwork/icon_target_vector.svg?v=5E-iconfix-002',
-  '../../viewer/artwork/GV-splash-0003.svg?v=5E-iconfix-002',
-  '../../viewer/GV-beta-0005E.py?v=5E-beautiful-001',
+  './manifest.webmanifest?v=5F-reticle-001',
+  '../../viewer/artwork/icon_target_vector.svg?v=5F-reticle-001',
+  '../../viewer/artwork/GV-splash-0003.svg?v=5F-reticle-001',
+  '../../viewer/artwork/GV-reticle-0001.svg?v=5F-reticle-001',
+  '../../viewer/GV-beta-0005F.py?v=5F-reticle-001',
   '../../discovery/beautiful-galaxy-catalog-beta.json?v=5E-beautiful-001',
   '../../discovery/galaxy-catalog-beta.json?v=5E-beautiful-001'
 ];
