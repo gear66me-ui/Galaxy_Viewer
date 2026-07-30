2858bc70b1f3927f692296a88e9315e75b0c3e0f
German Arciniegas <gear66me@gmail.com>
2026-07-29 19:30:56 -0500

    Cache standalone 5X beta release

--- mobile/beta/service-worker.js
+++ mobile/beta/service-worker.js
@@ -1,16 +1,16 @@
-const CACHE_NAME='galaxy-viewer-beta-5W-green-target-001';
+const CACHE_NAME='galaxy-viewer-beta-5X-full-green-001';
 const APP_SHELL=[
   './',
   './index.html',
-  './manifest.webmanifest?v=5W-green-target-001',
-  '../../viewer/artwork/icon_target_vector.svg?v=5W-green-target-001',
-  '../../viewer/artwork/icon_transparent.png?v=5W-green-target-001',
-  '../../viewer/artwork/GV-splash-0003.svg?v=5W-green-target-001',
-  '../../viewer/artwork/GV-reticle-0001.svg?v=5W-green-target-001',
-  '../../viewer/GV-beta-0005W.py?v=5W-green-target-001',
-  '../../viewer/GV-beta-0005R-comet-120.css?v=5W-green-target-001',
-  '../../discovery/beautiful-galaxy-catalog-beta.json?v=5W-green-target-001',
-  '../../discovery/galaxy-catalog-beta.json?v=5W-green-target-001'
+  './manifest.webmanifest?v=5X-full-green-001',
+  '../../viewer/artwork/icon_target_vector.svg?v=5X-full-green-001',
+  '../../viewer/artwork/icon_transparent.png?v=5X-full-green-001',
+  '../../viewer/artwork/GV-splash-0003.svg?v=5X-full-green-001',
+  '../../viewer/artwork/GV-reticle-0001.svg?v=5X-full-green-001',
+  '../../viewer/GV-beta-0005X.py?v=5X-full-green-001',
+  '../../viewer/GV-beta-0005R-comet-120.css?v=5X-full-green-001',
+  '../../discovery/beautiful-galaxy-catalog-beta.json?v=5X-full-green-001',
+  '../../discovery/galaxy-catalog-beta.json?v=5X-full-green-001'
 ];
 
 self.addEventListener('install',event=>{
