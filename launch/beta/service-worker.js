1cc2cd8e44feb349c84ddc4de05acb1f2da8b4d9
German Arciniegas <gear66me@gmail.com>
2026-07-30 22:52:07 -0500

    Add beta launcher service worker

---
+++ launch/beta/service-worker.js
@@ -0,0 +1,18 @@
+const GV_LAUNCHER_SW_VERSION = 'galaxy-viewer-beta-launcher-6i-001';
+
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('message', event => {
+  if (event.data === 'SKIP_WAITING') self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request, { cache: 'no-store' }));
+});
