1c9c7e284a17c7d7e95065b99df153243313d039
German Arciniegas <gear66me@gmail.com>
2026-07-31 18:23:43 -0500

    Update beta launcher service worker to 6O

--- launch/beta/service-worker.js
+++ launch/beta/service-worker.js
@@ -1,4 +1,4 @@
-const GV_LAUNCHER_SW_VERSION = 'galaxy-viewer-beta-launcher-6n-001';
+const GV_LAUNCHER_SW_VERSION = 'galaxy-viewer-beta-launcher-6o-001';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
