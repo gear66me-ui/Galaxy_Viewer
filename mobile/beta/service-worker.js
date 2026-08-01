c47f700e8530a4212f4226fb0220f38fabaaba13
German Arciniegas <gear66me@gmail.com>
2026-08-01 15:01:17 -0500

    Update mobile beta service worker to 6W

--- mobile/beta/service-worker.js
+++ mobile/beta/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='galaxy-viewer-beta-6v-resume-hamburger-coordinate-001';
+const CACHE_NAME='galaxy-viewer-beta-6w-metallic-resume-001';
 
 const clearAllCaches=()=>caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key))));
 
