d445121682121585600fd070950d6e7bb5f85ea2
German Arciniegas <gear66me@gmail.com>
2026-09-26 08:43:04 -0500

    Build 0008: compact travel distance presentation

--- viewer/GV-beta-200-009.py
+++ viewer/GV-beta-200-009.py
@@ -83,7 +83,7 @@ display(Javascript(r"""
 (async()=>{
 'use strict';
 const VERSION='GV-beta-200-009';
-const GV200001_BUILD='0007';
+const GV200001_BUILD='0008';
 const GV_RUNTIME='0082';
 const fresh=url=>`${url}?v=GV200001-${GV200001_BUILD}`;
 window.GV_BOOT_CONFIG=Object.freeze({
